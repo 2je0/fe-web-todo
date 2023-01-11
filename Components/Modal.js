a7a3b38cc641c5095570e369a07b48ae096df450
RentalHub <rentalhub@RentalHubui-noteubug.local>
2023-01-11 10:41:09 +0900

    chore: 안쓰는 파일 삭제

--- Components/Modal.js
+++
@@ -1,8 +0,0 @@
-const modalContainer = document.querySelector('.modal-container');
-export const modalShow = () => {
-  modalContainer.classList.remove('modal-hidden');
-};
-
-export const modalHide = () => {
-  modalContainer.classList.add('modal-hidden');
-};
--- index.js
+++
@@ -1,7 +0,0 @@
-import { State } from './store.js';
-import { EventHandler } from './Event/Events.js';
-
-const state = new State();
-EventHandler(state);
-
-// updateToDoListUI(state);
